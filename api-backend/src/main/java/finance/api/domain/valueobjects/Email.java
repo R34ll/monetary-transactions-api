674ae1e5e9ebb42fa543165bb8f680dd713ce311
Miquéias Moreira <xMiqueiasX202@Gmail.com>
2025-07-31 14:50:54 +0000

    feat(domain): add Email value object and its unit tests

--- api-backend/src/main/java/finance/api/domain/valueobjects/Email.java
+++ api-backend/src/main/java/finance/api/domain/valueobjects/Email.java
@@ -1,3 +1,14 @@
 public class Email {
+    private final String email;
     
+    public Email(String email){
+        if (email == null || !email.matches("^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$")) {
+            throw new IllegalArgumentException("Invalid email format");
+        }
+        this.email = email;
+    }
+
+    public String getValue() {
+        return email;
+    }
 }
