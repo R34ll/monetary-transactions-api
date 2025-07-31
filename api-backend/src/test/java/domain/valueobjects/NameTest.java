e0bc543c638a52fc3bddd8e8850c0e13f4300591
Miquéias Moreira <xMiqueiasX202@Gmail.com>
2025-07-31 14:46:59 +0000

    test(domain): Name tests

---
+++ api-backend/src/test/java/domain/valueobjects/NameTest.java
@@ -0,0 +1,68 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+public class NameTest{
+    @Test
+    void validNameShouldBeAccepted(){
+        Name name = new Name("John Doe");
+        assertEquals("John Doe", name.getValue());
+    }
+
+    @Test
+    void nameCannotBeNull(){
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Name(null);
+        });
+        
+        assertEquals("Name cannot be null or blank.", exception.getMessage());
+
+    }
+
+    @Test
+    void nameCannotBeBlank() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Name("   ");
+        });
+        assertEquals("Name cannot be null or blank.", exception.getMessage());
+    }
+
+    @Test
+    void nameCannotBeLongerThan250Characters() {
+        String longName = "a".repeat(251);
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Name(longName);
+        });
+        assertEquals("Name cannot be longer than 250 characters.", exception.getMessage());
+    }
+
+    @Test
+    void nameCannotContainNumbers() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Name("John Doe123");
+        });
+        assertEquals("Name must not contain numbers or special characters.", exception.getMessage());
+    }
+
+    @Test
+    void nameCannotContainSpecialCharacters() {
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Name("John@Doe!");
+        });
+        assertEquals("Name must not contain numbers or special characters.", exception.getMessage());
+    }
+
+    @Test
+    void nameWithAccentedCharactersIsAccepted() {
+        Name name = new Name("José Álvares");
+        assertEquals("José Álvares", name.getValue());
+    }
+
+    @Test
+    void nameIsTrimmed() {
+        Name name = new Name("  John Doe  ");
+        assertEquals("John Doe", name.getValue());
+    }
+
+}
