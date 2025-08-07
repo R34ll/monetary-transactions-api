716acda6c1145ca5d2718ad4c89df444321d74b3
Miquéias Moreira <xMiqueiasX202@Gmail.com>
2025-08-07 23:53:35 +0000

    feat: add throwIf* validation methods structure for TransferMoneyToAccount

---
+++ api-backend/src/test/java/application/usecases/CreateTransferMoneyToAccountTest.java
@@ -0,0 +1,13 @@
+package finance.api.application;
+
+class TransferMoneyToAccount {
+
+    public void throwIfUserDoesNotExist(){}
+    public void throwIfAccountDoesNotExist(){}
+    public void throwIfSameAccount(){}
+    public void throwIfAccountInactive(){}
+    public void throwIfInsufficientBalance(){}
+    public void throwIfInvalidAmount(){}
+
+
+}
