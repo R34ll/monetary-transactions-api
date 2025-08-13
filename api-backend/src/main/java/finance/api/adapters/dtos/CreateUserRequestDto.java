36304e16792118910a0fd12aa2d6d4f2c9bc6cc8
Miquéias Moreira <xMiqueiasX202@Gmail.com>
2025-08-13 19:25:52 +0000

    fix: Change to use string instead of valueobjects

--- api-backend/src/main/java/finance/api/adapters/dtos/CreateUserRequestDto.java
+++ api-backend/src/main/java/finance/api/adapters/dtos/CreateUserRequestDto.java
@@ -1,9 +1,4 @@
 package finance.api.adapters.dtos;
 
-import finance.api.domain.valueobjects.Email;
-import finance.api.domain.valueobjects.Name;
-import finance.api.domain.valueobjects.Password;
-import finance.api.domain.valueobjects.Document;
 
-
-public record CreateUserRequestDto(Name name, Email email, Password password, Document document){}
+public record CreateUserRequestDto(String name, String email, String password, String document){}
